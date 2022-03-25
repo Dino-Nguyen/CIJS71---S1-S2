807ce113739adb7fa18451f813c625224e179b48
Dino-Nguyen <xiao.dinnie@gmail.com>
2022-03-26 05:59:28 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,37 @@
+
+function reverseStr() {
+    document.getElementById("outputStr").innerHTML =''
+    let inputStr = document.getElementById("reverseStr").value 
+    
+    document.getElementById("outputStr").innerHTML += inputStr.split("").reverse().join("")
+  }
+  
+  function unique() {
+    document.getElementById('outputArr1').innerHTML = ''
+  arr1 = document.getElementById('inputArr1').value
+  arr = arr1.split("")
+  arr= Array.from(new Set(arr))   
+  document.getElementById('outputArr1').innerHTML += arr
+  }
+
+ function count() {
+    document.getElementById('outputArr2').innerHTML = ''
+    arr2 = document.getElementById('inputArr2').value
+    arr = arr2.split("")
+    arr= arr.sort()
+    number = 0
+    times = 0
+    counting = 1
+    for (let i= arr.length-1; i > 0; --i){
+        if (arr[i] == arr[i-1]) {
+        counting ++; }
+        else {
+             if (times < counting) {
+                 number= arr[i]
+                 times = counting
+             }
+        
+     counting = 1
+    }}
+    document.getElementById('outputArr2').innerHTML += `Số ` +number +` xuất hiện nhiều nhất là `+ times +` lần`
+ } 
