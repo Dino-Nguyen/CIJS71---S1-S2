d0ec57090727ad6932b370f5594fc8d902e42089
Dino-Nguyen <xiao.dinnie@gmail.com>
2022-03-29 07:54:22 +0700

    last commit

--- js/index.js
+++ js/index.js
@@ -39,34 +39,130 @@ function reverseStr() {
  ListNumber = [
    {
      name : 'Alice',
-     number : 94758052,
+     number : '94758052',
    },
    {
     name : 'Bob',
-    number : 94758052,
+    number : '94778052',
   },
   {
     name : 'Cris',
-    number : 94758052,
+    number : '94456052',
   },
   {
     name : 'Dan',
-    number : 94758052,
+    number : '94758052',
   },
  ]
 
+ //let addName = document.getElementById("addName").value
+ //addName = addName.toString()
+ //let addNumber = document.getElementById("addNumber").value
+
+ //addList.name = document.getElementById("addName").value
+ //addList.number = document.getElementById("addNumber").value
+ 
+ function addToList() {
+  const addList = {}
+  addList.name = document.getElementById("addName").value
+ addList.number = document.getElementById("addNumber").value
+
+//  Object.defineProperties(addList,
+ //  {name : {value : document.getElementById("addName").value}},
+// {number : {value :  document.getElementById("addNumber").value}})
+        
+        if (addList.name ==="") {
+             alert("Please enter Name")
+             return
+        }
+        if (addList.number ===""){
+          alert("Please enter numbers")
+          return
+        }
+        else {
+          ListNumber.push(addList)
+        
+        console.log(ListNumber)
+          for (let i=0 ; i < ListNumber.length -1 ; i++ ) {
+            for ( let j = i + 1 ; j < ListNumber.length; j++) {
+            if (ListNumber[i].name === ListNumber[j].name){
+                ListNumber.splice([j],1)
+                alert("Already exist this name")
+            }
+             if (ListNumber[i].number === ListNumber[j].number){
+              ListNumber.splice([j],1)
+              alert("Already exist this number")
+            }
+          }
+        }}
+          document.getElementById("listNumber").innerHTML =''
+        renderListNumber()
+ }
+
  function renderListNumber() {
+  const ar = ListNumber.sort(function(a, b)
+  {
+    let nA = a.name.toLowerCase();
+    let nB = b.name.toLowerCase();
+   
+    if(nA < nB)
+      return -1;
+    else if(nA > nB)
+      return 1;
+   return 0;
+  });
    for (let listName of ListNumber) {
+ 
      document.getElementById("listNumber").innerHTML += `
          <div class="nameItem"> 
            <span class="name"> `+ listName.name +`
            </span>
            <span class="number"> `+ listName.number +`
-           <button> Del </button>
+          
            </span>
            
          </div>
      `
+    
+    
    }
+   
  }
  renderListNumber()
+
+function delDupNum() {
+ for (let i=0 ; i < ListNumber.length -1 ; i++ ) {
+  for ( let j = i + 1 ; j < ListNumber.length; j++){
+    if (ListNumber[i].number === ListNumber[j].number){
+      ListNumber.splice([j],1)
+     alert("Dan and Alice have the same number")
+   }
+   }
+}
+document.getElementById("listNumber").innerHTML =''
+renderListNumber()
+}
+
+function search() {
+  find = document.getElementById("findName").value
+  console.log(find)
+  for (let listName of ListNumber) {
+    if (find ===""){
+      alert("You haven't enter infomation")
+      return
+    }
+  if (listName.name === find || listName.number === find) {
+    document.getElementById("listNumber").innerHTML =''
+    document.getElementById("listNumber").innerHTML += `
+         <div class="nameItem"> 
+           <span class="name"> `+ listName.name +`
+           </span>
+           <span class="number"> `+ listName.number +`
+          
+           </span>
+           
+         </div>
+     `
+  }
+}
+}
